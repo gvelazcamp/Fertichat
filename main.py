774e787f432bf1b0e3a8d658892392c16dc1192b
gvelazcamp <97753660+gvelazcamp@users.noreply.github.com>
2026-01-06 13:38:30 -0300

    Add import for normalizar_texto function

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ from familias import mostrar_familias
 from comprobantes import mostrar_menu_comprobantes
 from ui_chat_chainlit import mostrar_chat_chainlit
 from sql_core import get_lista_proveedores, get_valores_unicos, ejecutar_consulta
+from ia_interpretador import normalizar_texto
 
 from ui_css import CSS_GLOBAL
 
